e5397beffcb4eee1a7a72c3215e12c5bd35bb74e
Adrien Rosi <adrienrosi@gmail.com>
2025-03-17 10:35:14 +0100

    Enveloppe: Ajout des scripts SQL et adaptation des schémas

--- schemas/enveloppe/enveloppe.sql
+++ schemas/enveloppe/enveloppe.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS dpe_audit.enveloppe;
 
 CREATE TYPE exposition as ENUM('exposition_simple', 'exposition_multiple');
 
+CREATE TYPE inertie as ENUM('lourde', 'légère');
+
 CREATE TABLE dpe_audit.enveloppe (
     dpe_audit_id UUID PRIMARY KEY,
     exposition exposition NOT NULL,
@@ -10,8 +12,19 @@ CREATE TABLE dpe_audit.enveloppe (
 );
 
 CREATE TABLE dpe_audit.refend (
-    dpe_audit_id UUID PRIMARY KEY,
-    exposition exposition NOT NULL,
-    q4pa_conv FLOAT
+    id UUID PRIMARY KEY,
+    dpe_audit_id UUID NOT NULL,
+    description TEXT NOT NULL,
+    surface FLOAT NOT NULL,
+    inertie inertie
+    --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
+);
+
+CREATE TABLE dpe_audit.plancher_intermediaire (
+    id UUID PRIMARY KEY,
+    dpe_audit_id UUID NOT NULL,
+    description TEXT NOT NULL,
+    surface FLOAT NOT NULL,
+    inertie inertie
     --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
 );
