59e1a5fc78e3759c42d3771914ce317bbd2d1220
Adrien Rosi <adrienrosi@gmail.com>
2025-05-15 10:49:42 +0200

    [enveloppe] : modification des déclarations SQL & lint

--- schemas/enveloppe/pont_thermique/pont_thermique.sql
+++ schemas/enveloppe/pont_thermique/pont_thermique.sql
@@ -18,14 +18,8 @@ CREATE TABLE dpe_audit.enveloppe_pont_thermique (
     type_liaison type_liaison NOT NULL,
     pont_thermique_partiel BOOLEAN,
     mur_id UUID NOT NULL,
-    plancher_bas_id UUID,
-    plancher_haut_id UUID,
-    baie_id UUID porte_id UUID
+    plancher_id UUID,
+    ouverture_id UUID
     -- Reference
     --FOREIGN KEY (enveloppe_id) REFERENCES dpe_audit.enveloppe(id)
-    --FOREIGN KEY (mur_id) REFERENCES dpe_audit.enveloppe_mur(id)
-    --FOREIGN KEY (plancher_bas_id) REFERENCES dpe_audit.enveloppe_plancher_bas(id)
-    --FOREIGN KEY (plancher_haut_id) REFERENCES dpe_audit.enveloppe_plancher_haut(id)
-    --FOREIGN KEY (baie_id) REFERENCES dpe_audit.enveloppe_baie(id)
-    --FOREIGN KEY (porte_id) REFERENCES dpe_audit.enveloppe_baie(id)
 );
