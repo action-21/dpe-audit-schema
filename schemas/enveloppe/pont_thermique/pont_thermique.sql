fceb79762554894c9a9bb5fca0809039b344b8f7
Adrien Rosi <adrienrosi@gmail.com>
2025-05-15 10:38:43 +0200

    [enveloppe] : modification des déclarations SQL & lint

--- schemas/enveloppe/pont_thermique/pont_thermique.sql
+++ schemas/enveloppe/pont_thermique/pont_thermique.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS dpe_audit.pont_thermique;
+DROP TABLE IF EXISTS dpe_audit.enveloppe_pont_thermique;
 
-CREATE TYPE type_pose as ENUM(
+CREATE TYPE type_liaison as ENUM(
     'plancher_bas_mur',
     'plancher_haut_mur',
     'refend_mur',
@@ -8,18 +8,24 @@ CREATE TYPE type_pose as ENUM(
     'menuiserie_mur'
 );
 
-CREATE TABLE dpe_audit.pont_thermique (
+CREATE TABLE dpe_audit.enveloppe_pont_thermique (
     id UUID PRIMARY KEY NOT NULL,
-    dpe_audit_id UUID NOT NULL,
+    enveloppe_id UUID NOT NULL,
     description TEXT NOT NULL,
     longueur FLOAT NOT NULL,
     kpt FLOAT,
     -- Liaison
-    type_liaison type_pose NOT NULL,
+    type_liaison type_liaison NOT NULL,
     pont_thermique_partiel BOOLEAN,
     mur_id UUID NOT NULL,
-    plancher_id UUID,
-    ouverture_id UUID
+    plancher_bas_id UUID,
+    plancher_haut_id UUID,
+    baie_id UUID porte_id UUID
     -- Reference
-    --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
+    --FOREIGN KEY (enveloppe_id) REFERENCES dpe_audit.enveloppe(id)
+    --FOREIGN KEY (mur_id) REFERENCES dpe_audit.enveloppe_mur(id)
+    --FOREIGN KEY (plancher_bas_id) REFERENCES dpe_audit.enveloppe_plancher_bas(id)
+    --FOREIGN KEY (plancher_haut_id) REFERENCES dpe_audit.enveloppe_plancher_haut(id)
+    --FOREIGN KEY (baie_id) REFERENCES dpe_audit.enveloppe_baie(id)
+    --FOREIGN KEY (porte_id) REFERENCES dpe_audit.enveloppe_baie(id)
 );
