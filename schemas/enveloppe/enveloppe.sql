ef5992d46188989e73805429fabacaec555ac4f6
Adrien Rosi <adrienrosi@gmail.com>
2025-05-15 10:51:32 +0200

    [enveloppe] : modification des déclarations SQL

--- schemas/enveloppe/enveloppe.sql
+++ schemas/enveloppe/enveloppe.sql
@@ -5,26 +5,25 @@ CREATE TYPE exposition as ENUM('exposition_simple', 'exposition_multiple');
 CREATE TYPE inertie as ENUM('lourde', 'légère');
 
 CREATE TABLE dpe_audit.enveloppe (
-    dpe_audit_id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY,
     exposition exposition NOT NULL,
     q4pa_conv FLOAT
-    --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
 );
 
-CREATE TABLE dpe_audit.refend (
+CREATE TABLE dpe_audit.enveloppe_refend (
     id UUID PRIMARY KEY,
-    dpe_audit_id UUID NOT NULL,
+    enveloppe_id UUID NOT NULL,
     description TEXT NOT NULL,
     surface FLOAT NOT NULL,
     inertie inertie
-    --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
+    --FOREIGN KEY (enveloppe_id) REFERENCES dpe_audit.enveloppe(id)
 );
 
-CREATE TABLE dpe_audit.plancher_intermediaire (
+CREATE TABLE dpe_audit.enveloppe_plancher_intermediaire (
     id UUID PRIMARY KEY,
-    dpe_audit_id UUID NOT NULL,
+    enveloppe_id UUID NOT NULL,
     description TEXT NOT NULL,
     surface FLOAT NOT NULL,
     inertie inertie
-    --FOREIGN KEY (dpe_audit_id) REFERENCES dpe_audit.audit(id)
+    --FOREIGN KEY (enveloppe_id) REFERENCES dpe_audit.enveloppe(id)
 );
